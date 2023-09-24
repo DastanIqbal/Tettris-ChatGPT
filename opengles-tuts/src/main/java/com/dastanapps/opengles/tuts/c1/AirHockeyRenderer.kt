df33a6807e974ce92aae9c1ea377b1f332c51f52
iqbal <iqbal.ahmed@xische.com>
2023-09-24 23:41:51 +0400

    feat added triangle fans

--- opengles-tuts/src/main/java/com/dastanapps/opengles/tuts/c1/AirHockeyRenderer.kt
+++ opengles-tuts/src/main/java/com/dastanapps/opengles/tuts/c1/AirHockeyRenderer.kt
@@ -2,6 +2,9 @@ package com.dastanapps.opengles.tuts.c1
 
 import android.content.Context
 import android.opengl.GLES20
+import android.opengl.GLES20.GL_FLOAT
+import android.opengl.GLES20.glGetAttribLocation
+import android.opengl.GLES20.glVertexAttribPointer
 import android.opengl.GLSurfaceView
 import com.dastanapps.opengles.tuts.LoggerConfig
 import com.dastanapps.opengles.tuts.ShaderHelper
@@ -24,25 +27,87 @@ class AirHockeyRenderer(
     private val POSITION_COMPONENT_COUNT = 2
     private val BYTES_PER_FLOAT = 4
 
+//    var tableVerticesWithTriangles = floatArrayOf(
+//        // Triangle 1
+//        -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f,
+//        // Triangle 2
+//        -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,
+//        // Line 1
+//        -0.5f, 0f, 0.5f, 0f,
+//        // Mallets
+//        0f, -0.25f, 0f, 0.25f
+//    )
+
     var tableVerticesWithTriangles = floatArrayOf(
-        // Triangle 1
-        -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f,
-        // Triangle 2
-        -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,
+        // Order of coordinates: X, Y, R, G, B
+        // Triangle Fan
+        0f,
+        0f,
+        1f,
+        1f,
+        1f,
+        -0.5f,
+        -0.5f,
+        0.7f,
+        0.7f,
+        0.7f,
+        0.5f,
+        -0.5f,
+        0.7f,
+        0.7f,
+        0.7f,
+        0.5f,
+        0.5f,
+        0.7f,
+        0.7f,
+        0.7f,
+        -0.5f,
+        0.5f,
+        0.7f,
+        0.7f,
+        0.7f,
+        -0.5f,
+        -0.5f,
+        0.7f,
+        0.7f,
+        0.7f,
         // Line 1
-        -0.5f, 0f, 0.5f, 0f,
+        -0.5f,
+        0f,
+        1f,
+        0f,
+        0f,
+        0.5f,
+        0f,
+        1f,
+        0f,
+        0f,
         // Mallets
-        0f, -0.25f, 0f, 0.25f
+        0f,
+        -0.25f,
+        0f,
+        0f,
+        1f,
+        0f,
+        0.25f,
+        1f,
+        0f,
+        0f
     )
 
     private var vertexShaderSource = """
-        attribute vec4 a_Position;     		
-
-        void main()                    
-        {                              
+        attribute vec4 a_Position;
+        attribute vec4 a_Color;
+        
+        varying vec4 v_Color;
+        
+        void main()
+        {
+            v_Color = a_Color;
+            
             gl_Position = a_Position;
             gl_PointSize = 10.0;
-        }   
+        }
 
     """.trimIndent()
 
@@ -51,17 +116,20 @@ class AirHockeyRenderer(
 
     private var fragmentShaderSource = """
         precision mediump float; 
-      	 								
-        uniform vec4 u_Color;          	   								
-          
-        void main()                    		
-        {                              	
-            gl_FragColor = u_Color;                                  		
+        
+        varying vec4 v_Color;
+        
+        void main()
+        {
+            gl_FragColor = v_Color;
         }
     """.trimIndent()
 
-    private val U_COLOR: String = "u_Color"
-    private var uColorLocation = 0
+    private val A_COLOR: String = "a_Color"
+    private val COLOR_COMPONENT_COUNT: Int = 3
+    private val STRIDE: Int = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT
+    private var aColorLocation = 0
+
 
     private var program = 0
 
@@ -87,17 +155,26 @@ class AirHockeyRenderer(
 
         GLES20.glUseProgram(program)
 
-        uColorLocation = GLES20.glGetUniformLocation(program, U_COLOR)
-        aPositionLocation = GLES20.glGetAttribLocation(program, A_POSITION)
+        aColorLocation = glGetAttribLocation(program, A_COLOR)
+        aPositionLocation = glGetAttribLocation(program, A_POSITION)
 
 
         // Bind our data, specified by the variable vertexData, to the vertex
         // attribute at location A_POSITION_LOCATION.
         vertexData.position(0)
-        GLES20.glVertexAttribPointer(
-            aPositionLocation, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT, false, 0, vertexData
+        glVertexAttribPointer(
+            aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData
         )
+
         GLES20.glEnableVertexAttribArray(aPositionLocation)
+
+        vertexData.position(POSITION_COMPONENT_COUNT)
+        glVertexAttribPointer(
+            aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT,
+            false, STRIDE, vertexData
+        )
+
+        GLES20.glEnableVertexAttribArray(aColorLocation)
     }
 
     override fun onSurfaceChanged(glUnused: GL10?, width: Int, height: Int) {
@@ -110,19 +187,15 @@ class AirHockeyRenderer(
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
 
         // Draw the table.
-        GLES20.glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f)
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6)
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6)
 
         // Draw the center dividing line.
-        GLES20.glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f)
         GLES20.glDrawArrays(GLES20.GL_LINES, 6, 2)
 
         // Draw the first mallet blue.
-        GLES20.glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f)
         GLES20.glDrawArrays(GLES20.GL_POINTS, 8, 1)
 
         // Draw the second mallet red.
-        GLES20.glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f)
         GLES20.glDrawArrays(GLES20.GL_POINTS, 9, 1)
     }
 
